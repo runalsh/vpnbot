286533eca39993f9729bd0359576ea35728227d5
runalsh <21105673+runalsh@users.noreply.github.com>
2024-05-19 10:45:59 +0300

    Revert "Merge branch 'upstream-angie-selfuse' into master"
    This reverts commit 5be83082231207d9e24a5df2638a7859bc2625a9, reversing
    changes made to c2de330195b1116cf4f43b1177c4b5f6a4ea8433.

--- scripts/start_ng.sh
+++ scripts/start_ng.sh
@@ -1,15 +1,8 @@
 cat /ssh/key.pub > /root/.ssh/authorized_keys
 ssh-keygen -A
 exec /usr/sbin/sshd -D -e "$@" &
-sed "s/ss:[0-9]\+/ss:$SSPORT/" /angie_default.conf > change_port
-cat change_port > /angie_default.conf
-sed "s/ss:[0-9]\+/ss:$SSPORT/" /etc/angie/angie.conf > change_port
-cat change_port > /etc/angie/angie.conf
-HOSTIP=$(route | awk '/default/ { print $2 }')
-sed -i "s/172.17.0.1/$HOSTIP/" /etc/angie/angie.conf
-if [ -z $AUTHUSER ]; then
-    touch /etc/angie/.htpasswd
-  else
-    htpasswd -c -b /etc/angie/.htpasswd $AUTHUSER $AUTHPASSWD
-fi
-angie -g "daemon off;"
+sed "s/ss:[0-9]\+/ss:$SSPORT/" /nginx_default.conf > change_port
+cat change_port > /nginx_default.conf
+sed "s/ss:[0-9]\+/ss:$SSPORT/" /etc/nginx/nginx.conf > change_port
+cat change_port > /etc/nginx/nginx.conf
+nginx -g "daemon off;"
--- scripts/start_upstream.sh
+++ scripts/start_upstream.sh
@@ -1,11 +1,4 @@
 cat /ssh/key.pub > /root/.ssh/authorized_keys
 ssh-keygen -A
 exec /usr/sbin/sshd -D -e "$@" &
-HOSTIP=$(route | awk '/default/ { print $2 }')
-sed -i "s/172.17.0.1/$HOSTIP/" /etc/angie/angie.conf
-if [ -z $AUTHUSER ]; then
-    touch /etc/angie/.htpasswd
-  else
-    htpasswd -c -b /etc/angie/.htpasswd $AUTHUSER $AUTHPASSWD
-fi
-angie -g "daemon off;"
+nginx -g "daemon off;"
